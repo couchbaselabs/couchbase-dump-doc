e4fb70ae6d8e9ef21628156ea7bbebc5ef8245ff
Graham Pople <grahampople@gmail.com>
2024-07-03 12:03:27 +0100

    Java8 and uberjar support

--- src/main/java/com/couchbase/test/Main.java
+++ src/main/java/com/couchbase/test/Main.java
@@ -1,12 +1,9 @@
 package com.couchbase.test;
 
-import com.couchbase.client.java.Cluster;
-import com.couchbase.client.java.ClusterOptions;
+import com.couchbase.client.java.*;
+import com.couchbase.client.java.json.JsonArray;
 import com.couchbase.client.java.json.JsonObject;
-import com.couchbase.client.java.kv.LookupInSpec;
-import com.couchbase.client.java.kv.MutateInOptions;
-import com.couchbase.client.java.kv.MutateInSpec;
-import com.couchbase.client.java.kv.StoreSemantics;
+import com.couchbase.client.java.kv.*;
 import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +19,7 @@ public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        var options = new Options();
+        Options options = new Options();
 
         options.addRequiredOption("h", "host", true, "Couchbase cluster hostname");
         options.addRequiredOption("u", "username", true, "Couchbase cluster username");
@@ -32,7 +29,7 @@ public class Main {
         options.addRequiredOption("c", "collection", true, "Couchbase collection name");
         options.addRequiredOption("d", "docId", true, "Couchbase document ID");
 
-        var parser = new DefaultParser();
+        CommandLineParser parser = new DefaultParser();
         CommandLine cmd;
         try {
             cmd = parser.parse(options, args);
@@ -43,35 +40,35 @@ public class Main {
             return;
         }
 
-        var hostname = cmd.getOptionValue("host");
-        var username = cmd.getOptionValue("username");
-        var password = cmd.getOptionValue("password");
-        var bucketName = cmd.getOptionValue("bucket");
-        var scopeName = cmd.getOptionValue("scope");
-        var collectionName = cmd.getOptionValue("collection");
-        var docId = cmd.getOptionValue("docId");
+        String hostname = cmd.getOptionValue("host");
+        String username = cmd.getOptionValue("username");
+        String password = cmd.getOptionValue("password");
+        String bucketName = cmd.getOptionValue("bucket");
+        String scopeName = cmd.getOptionValue("scope");
+        String collectionName = cmd.getOptionValue("collection");
+        String docId = cmd.getOptionValue("docId");
 
         LOGGER.info("Connecting to cluster {} with user {}", hostname, username);
 
-        try (var cluster = Cluster.connect(hostname, ClusterOptions.clusterOptions(username, password)
+        try (Cluster cluster = Cluster.connect(hostname, ClusterOptions.clusterOptions(username, password)
                 .environment(env -> env.securityConfig(sec ->
                         sec.enableCertificateVerification(false))))) {
             cluster.waitUntilReady(Duration.ofSeconds(30));
-            var bucket = cluster.bucket(bucketName);
-            var scope = bucket.scope(scopeName);
-            var collection = scope.collection(collectionName);
-            var bodyFilename = docId + ".content.bin";
+            Bucket bucket = cluster.bucket(bucketName);
+            Scope scope = bucket.scope(scopeName);
+            Collection collection = scope.collection(collectionName);
+            String bodyFilename = docId + ".content.bin";
 
             LOGGER.info("Reading doc {} from {}.{}.{}", docId, bucketName, scopeName, collectionName);
 
-            var result = collection.lookupIn(docId, List.of(
+            LookupInResult result = collection.lookupIn(docId, List.of(
                     LookupInSpec.get("$document").xattr(),
                     LookupInSpec.get("$XTOC").xattr(),
                     LookupInSpec.get("")));
 
-            var documentVattr = result.contentAsObject(0);
-            var xtocVattr = result.contentAsArray(1);
-            var bodyContent = result.contentAsBytes(2);
+            JsonObject documentVattr = result.contentAsObject(0);
+            JsonArray xtocVattr = result.contentAsArray(1);
+            byte[] bodyContent = result.contentAsBytes(2);
 
             LOGGER.info("$document contents: {}", documentVattr);
             LOGGER.info("$XTOC contents: {}", xtocVattr);
@@ -83,10 +80,10 @@ public class Main {
             for (Object v : xtocVattr.toList()) {
                 String xattr = (String) v;
                 LOGGER.info("Fetching xattr: {}", xattr);
-                var xattrResult = collection.lookupIn(docId, List.of(
+                LookupInResult xattrResult = collection.lookupIn(docId, List.of(
                         LookupInSpec.get(xattr).xattr()));
-                var xattrContent = xattrResult.contentAsBytes(0);
-                var xattrFilename = docId + "." + xattr + ".content.bin";
+                byte[] xattrContent = xattrResult.contentAsBytes(0);
+                String xattrFilename = docId + "." + xattr + ".content.bin";
                 LOGGER.info("xattr {} length: {} bytes", xattr, xattrContent.length);
                 LOGGER.info("xattr {} content written to {}", xattr, xattrFilename);
 
